a63c3551da823ba36297e7c6ce14a3054927731e
IshtarAtelier <pisano.ishtar@gmail.com>
2026-06-13 12:20:52 -0300

    refactor: import React in blog [slug] page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+import React from 'react';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
